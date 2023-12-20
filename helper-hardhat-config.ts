a3ce317c4382282b4ec8c80b788b653be635f4a4
Prastav <pokharelprastav@gmail.com>
2023-12-20 09:49:14 +0545

    scripts for purpose, vote and queue-and-execute added

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -25,4 +25,4 @@ export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
 export const NEW_STORE_VALUE = 77;
 export const FUNC = "store";
-export const PROPOSAL_DESCRIPTION = "Proposal #1 77 in the Box!";
+export const PROPOSAL_DESCRIPTION = "Proposal #1 77 in the Store and Retrieve!";
